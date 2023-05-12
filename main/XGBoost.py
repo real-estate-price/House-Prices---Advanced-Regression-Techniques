fbdb8d63da46d14347df7ca7db8a49811dc94530
MishaVatulich <57654804+MishaVatulich@users.noreply.github.com>
2023-05-12 18:40:35 +1000

    Update XGBoost.py
    Чутка изменил метод обработки NAN

--- main/XGBoost.py
+++ main/XGBoost.py
@@ -6,16 +6,17 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 from xgboost import XGBRegressor
-"""Здесь используется модель XGBRegressor. Предобработка данных: NaN заменятеся на наиболее частое значение в столбце,
-а категориальные значения обрабатываются с помощью onehot.
-Ошибка: 14,3%
+"""Здесь используется модель XGBRegressor. Предобработка данных: удалил 4 признака, где слишком много пропущенных
+ данных, NaN заменятеся на наиболее частое значение в столбце, а категориальные значения обрабатываются 
+ с помощью onehot.
+Ошибка: 14,1%
 Замечания: ручной подбор параметров модели ухудшает точность. Поэтому дэфолтные значения вроде как самые оптимальные
 """
 
 
 file = pd.read_csv('train (2).csv', index_col=0)
 y = file.SalePrice
-X = file.drop(['SalePrice'], axis=1)
+X = file.drop(['SalePrice', 'Alley', 'PoolQC', 'Fence', 'MiscFeature'], axis=1)
 X_train, X_valid, y_train, y_valid = train_test_split(X, y,  train_size=0.8, test_size=0.2, random_state=0)
 
 """Выделяем категориальные и числовые данные"""
