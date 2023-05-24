b528bd07575d7281a438125e813585a5e2cb6296
MishaVatulich <57654804+MishaVatulich@users.noreply.github.com>
2023-05-24 21:01:02 +1000

    Update catboostReg.py
    Некоторые признаки отранжированы. Ошибка 12,7%

--- main/catboostReg.py
+++ main/catboostReg.py
@@ -16,11 +16,50 @@ from catboost import CatBoostRegressor
 на No... В остальных столбцах NaN заменятеся на наиболее частое значение в столбце;
 категориальные значения обрабатываются с помощью onehot; для подбора гипер параметров использовали optuna.
 
-Ошибка: 13,2%
+Ошибка: 12,7%
 """
 
 file1 = pd.read_csv('train (2).csv', index_col=0)
 file2 = pd.read_csv('test.csv', index_col=0)
+
+file1['LotShape'] = file1['LotShape'].replace(['Reg', 'IR1', 'IR2', 'IR3'], [4, 3, 2, 1]).astype('float')
+file1['Utilities'] = file1['Utilities'].replace(['AllPub', 'NoSewr', 'NoSeWa', 'ELO'], [4, 3, 2, 1]).astype('float')
+file1['LotConfig'] = file1['LotConfig'].replace(['Inside', 'Corner', 'CulDSac', 'FR2', 'FR3'], [2, 3, 1, 4, 5]).astype('float')
+file1['LandSlope'] = file1['LandSlope'].replace(['Gtl', 'Mod', 'Sev'], [3, 2, 1]).astype('float')
+file1['BldgType'] = file1['BldgType'].replace(['1Fam', '2fmCon', 'Duplex', 'TwnhsE', 'Twnhs'], [5, 3, 4, 2, 1]).astype('float')
+file1['ExterQual'] = file1['ExterQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['ExterCond'] = file1['ExterCond'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['BsmtQual'] = file1['BsmtQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['BsmtCond'] = file1['BsmtCond'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['BsmtExposure'] = file1['BsmtExposure'].replace(['Gd', 'Av', 'Mn', 'No'], [4, 3, 2, 1]).astype('float')
+file1['HeatingQC'] = file1['HeatingQC'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['Electrical'] = file1['Electrical'].replace(['SBrkr', 'FuseA', 'FuseF', 'FuseP', 'Mix'], [5, 4, 3, 2, 3]).astype('float')
+file1['KitchenQual'] = file1['KitchenQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['FireplaceQu'] = file1['FireplaceQu'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['GarageFinish'] = file1['GarageFinish'].replace(['Fin', 'RFn', 'Unf'], [3, 2, 1]).astype('float')
+file1['GarageQual'] = file1['GarageQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['GarageCond'] = file1['GarageCond'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file1['PavedDrive'] = file1['PavedDrive'].replace(['Y', 'P', 'N'], [3, 2, 1]).astype('float')
+
+file2['LotShape'] = file2['LotShape'].replace(['Reg', 'IR1', 'IR2', 'IR3'], [4, 3, 2, 1]).astype('float')
+file2['Utilities'] = file2['Utilities'].replace(['AllPub', 'NoSewr', 'NoSeWa', 'ELO'], [4, 3, 2, 1]).astype('float')
+file2['LotConfig'] = file2['LotConfig'].replace(['Inside', 'Corner', 'CulDSac', 'FR2', 'FR3'], [2, 3, 1, 4, 5]).astype('float')
+file2['LandSlope'] = file2['LandSlope'].replace(['Gtl', 'Mod', 'Sev'], [3, 2, 1]).astype('float')
+file2['BldgType'] = file2['BldgType'].replace(['1Fam', '2fmCon', 'Duplex', 'TwnhsE', 'Twnhs'], [5, 3, 4, 2, 1]).astype('float')
+file2['ExterQual'] = file2['ExterQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['ExterCond'] = file2['ExterCond'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['BsmtQual'] = file2['BsmtQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['BsmtCond'] = file2['BsmtCond'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['BsmtExposure'] = file2['BsmtExposure'].replace(['Gd', 'Av', 'Mn', 'No'], [4, 3, 2, 1]).astype('float')
+file2['HeatingQC'] = file2['HeatingQC'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['Electrical'] = file2['Electrical'].replace(['SBrkr', 'FuseA', 'FuseF', 'FuseP', 'Mix'], [5, 4, 3, 2, 3]).astype('float')
+file2['KitchenQual'] = file2['KitchenQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['FireplaceQu'] = file2['FireplaceQu'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['GarageFinish'] = file2['GarageFinish'].replace(['Fin', 'RFn', 'Unf'], [3, 2, 1]).astype('float')
+file2['GarageQual'] = file2['GarageQual'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['GarageCond'] = file2['GarageCond'].replace(['Ex', 'Gd', 'TA', 'Fa', 'Po'], [5, 4, 3, 2, 1]).astype('float')
+file2['PavedDrive'] = file2['PavedDrive'].replace(['Y', 'P', 'N'], [3, 2, 1]).astype('float')
+
 y_train = file1.SalePrice
 X_train = file1.drop(['SalePrice', 'Alley', 'PoolQC', 'Fence', 'MiscFeature', 'PoolArea',
                       'LowQualFinSF', 'MoSold', 'Condition2', 'LotConfig', 'YrSold', 'MiscVal',
@@ -36,15 +75,21 @@ num_cols = [cname for cname in X_train.columns if X_train[cname].dtype in ['int6
 
 for column in X_train.columns:
                 if column == 'FireplaceQu':
-                    X_train[column] = X_train[column].fillna('No Fireplace')
-                    X_test[column] = X_test[column].fillna('No Fireplace')
-                elif column == 'GarageYrBlt': 
                     X_train[column] = X_train[column].fillna(0)
                     X_test[column] = X_test[column].fillna(0)
-                elif column in ('GarageCond', 'GarageType', 'GarageFinish', 'GarageQual'):
-                    X_train[column] = X_train[column].fillna('No Garage')
-                    X_test[column] = X_test[column].fillna('No Garage')
-                elif column in ('BsmtExposure', 'BsmtQual', 'BsmtCond', 'BsmtFinType1'):
+                elif column == 'GarageYrBlt':
+                    X_train[column] = X_train[column].fillna(0)
+                    X_test[column] = X_test[column].fillna(0)
+                elif column in ('GarageCond', 'GarageFinish', 'GarageQual'):
+                    X_train[column] = X_train[column].fillna(0)
+                    X_test[column] = X_test[column].fillna(0)
+                elif column == 'GarageType':
+                    X_train[column] = X_train[column].fillna('No')
+                    X_test[column] = X_test[column].fillna('No')
+                elif column in ('BsmtExposure', 'BsmtQual', 'BsmtCond'):
+                    X_train[column] = X_train[column].fillna(0)
+                    X_test[column] = X_test[column].fillna(0)
+                elif column == 'BsmtFinType1':
                     X_train[column] = X_train[column].fillna('No Basement')
                     X_test[column] = X_test[column].fillna('No Basement')
                     
@@ -63,8 +108,8 @@ preprocessor = ColumnTransformer(  # применяем ко всем столб
         ('cat', categorical_transformer, cat_cols)
     ])
 
-model = CatBoostRegressor(learning_rate=0.14144651033395983, max_depth=3, colsample_bylevel=0.7573342483166768,
-                          boosting_type='Ordered', bootstrap_type='MVS', loss_function='RMSE', iterations=1000,
+model = CatBoostRegressor(learning_rate=0.03996902268554373, max_depth=9, colsample_bylevel=0.2710787847399107,
+                          boosting_type='Ordered', bootstrap_type='Bernoulli', subsample=0.10128815141985761, loss_function='RMSE', iterations=1000,
                           verbose=0, random_state=42)
 my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                               ('model', model)
